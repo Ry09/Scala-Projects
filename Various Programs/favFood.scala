30dcb290f38b73b93f4325615d847e9d2ba86dd5
Ry09 <xbloodyx9@gmail.com>
2016-11-29 20:41:18 -0500

    Updating 2 simple programs

---
+++ Various Programs/favFood.scala
@@ -0,0 +1,9 @@
+/* This is a program that takes input from the user and returns it in a sentence. In this case it is favorite foods. Note that error handling has not been applied here.
+*/
+
+print("\nWhat is your favorite food?")
+//note this is only expecting one favorite dish but will accept //everything
+
+val a = scala.io.StdIn.readLine()
+
+println("\n\nAhh so " + a + " is your favorite food!\n\n\n")
---
+++ Various Programs/simpleQuote.scala
@@ -0,0 +1,7 @@
+/* this is a simple program for me to get back into programming in scala. All this does it simply print a quote and where it is from.
+*/
+
+
+print("\n\nThe power to kill is the only power that matters. The power to kill and destroy, because if you can't kill then you are always subject to those who can, and nothing and no one will ever save you.")
+
+println("\n\t\t\t\t--Ender's Game")
